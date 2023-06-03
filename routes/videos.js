816bc0df9bbfb6cb066f2a28853dea1f37ee0ae4
Sweta <spmac@swetas-mbp.lan>
2023-06-03 17:41:07 -0400

    complete post video route to upload new videos

--- routes/videos.js
+++ routes/videos.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const videosData = require('../data/video-details.json');
-
-// console.log(videosData);
 
 router.get("/videos", (req, res) => {
     res.status(200).json(videosData.map((video) => {
@@ -23,26 +22,25 @@ router.get("/videos/:id", (req, res) => {
 
 
 router.post("/videos", (req, res) => {
-    const { title, channel, image, description, video, timestamp } = req.body;
+    const { title, description, timestamp } = req.body;
 
     const newVideo = {
         id: uuidv4(),
         title,
-        channel,
-        image,
+        channel: "My Channel",
+        image: "public/images/Upload-video-preview.jpg",
         description,
         views: 0, 
         likes: 0,
         duration: "5:10",
-        video,
+        video: "https://project-2-api.herokuapp.com/stream",
         timestamp,
         comments: [],
     }
-    // const newVideoString = JSON.stringify(newVideo);
-
+    
     videosData.push(newVideo);
-    res.json(newVideo);
+    fs.writeFileSync('./data/video-details.json', JSON.stringify(videosData));
+    res.status(201).json(newVideo);
 })
-
 
 module.exports = router;
